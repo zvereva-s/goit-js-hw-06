6ca56e2715a999deb88e49b24ea258b6c6d0b96d
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-05-20 23:22:18 +0300

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,64 +1,60 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-let quantityOfDivEl = 0;
-// function checkingInputvalue() {
-  const inputEl = document.querySelector('input');
-  const btnCreate = document.querySelector('[data-create]');
 
-  inputEl.addEventListener('input', () => {});
+function makingColorBoxes() {
+const inputEl = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+
+let number = 0;
+
+inputEl.addEventListener('input', (event) => {
+
+  number = event.currentTarget.value;
+  event.preventDefault();
+
   btnCreate.addEventListener('click', (event) => {
-  console.log();
+    createBoxes(number);
+  });
 });
 
-
-
-
-// }
+  btnDestroy.addEventListener('click', destroyBoxes);
+  
+}
 
 function createBoxes(amount) {
-  let boxEl;
+  
   const arrObj = [];
+  const listBoxes = [];
 
-  for (let i = 0; i <= amount; i += 1) {
-    boxEl = document.createElement('div');
-    arrObj.push({ boxEl });
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement('div');
+    boxEl.classList.add('color-box')
+  arrObj.push({ boxEl });
   }
   arrObj.map((el) => el.boxEl.style.backgroundColor = getRandomHexColor());
 
   let width = 30;
   let height = 30;
 
-  //на каждой итерации + 10пкс
-  //к-во 10 итераций * 10пикс
-
-  for (let i = 0; i <= 10 * amount; i += 1) {
+  arrObj.map(el => {
     width += 10;
-    arrObj.map(el => {
-      el.boxEl.style.width = width + 'px'
-    });
-
-    height += 10; 
-    arrObj.map((el) => el.boxEl.style.height = height + 'px');
+    height += 10;
+    el.boxEl.style.width = width + 'px';
+    el.boxEl.style.height = height + 'px'
+  });
+  
+  for (const obj of arrObj) {
+    listBoxes.push(obj.boxEl)
   }
-  console.log(arrObj[1].boxEl);
-  console.log(arrObj);
+  const divBoxesEl = document.querySelector('#boxes');
+  divBoxesEl.append(...listBoxes);
 }
-console.log(createBoxes(10));
 
+function destroyBoxes() {
+  const boxesEl = document.querySelectorAll('.color-box');
+  boxesEl.forEach(el => el.remove());
+}
 
-//! obj = { ==> скорее всего это массив объектов//
-// boxEl1: document.createElement('div'),
-// boxEl2: document.createElement('div'),
-//!}
-//! boxEl1.style.width = 30px;
-//! boxEl1.style.heigth = 30px;
-//! boxEl1.style.background-color = getRandomHexColor();
-//! boxEl2.style.width = 40px;
-//! boxEl2.style.heigth = 40px;
-//! boxEl2.style.background-color = getRandomHexColor();
-
-//TODO amount === length arr or obj === к-во итераций при переборе и добавлении стилей
-
-//? при переборе и добавлении стилей - шаблонная строка или просто +10 к значению или доп переменная
-
+makingColorBoxes();
